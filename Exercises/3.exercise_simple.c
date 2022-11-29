6e1f9afb95f8e2338c63b328433d5fc5aa44aa56
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-11-29 15:19:46 +0300

    Update 3.exercise_simple.c
    solved

--- Exercises/3.exercise_simple.c
+++ Exercises/3.exercise_simple.c
@@ -1,2 +1,12 @@
 // Take a single character  as input, you may benefit from scanf("%c", &ch ); and print("%c", ch) to print the character. 
 // Write a program to print the ASCII value of the given character.
+
+
+#include "stdio.h"
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    printf("ASCII value of %c = %d", ch, ch);
+    return 0;
+}
