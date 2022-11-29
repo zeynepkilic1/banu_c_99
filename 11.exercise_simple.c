6c58d9b8c94c4303176f2ddabbe91123a482b335
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-11-29 15:05:11 +0300

    Create 11.exercise_simple.c

---
+++ 11.exercise_simple.c
@@ -0,0 +1,2 @@
+// Take a single character  as input, you may benefit from scanf("%c", &ch ); and print("%c", ch) to print the character. 
+// Write a program to print the ASCII value of the given character.
