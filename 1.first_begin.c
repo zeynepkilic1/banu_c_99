65e6ac4feed2f391a854cf8ebe6d1761d2b940e3
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-11-29 09:38:52 +0300

    Create 1.first_begin.c

---
+++ 1.first_begin.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char *argv[]) {
+    int number = 5;      // integer variable
+    number = 5.5;        // error
+    double number;       // error
+    const double PI = 3.14; // constant variable
+    PI = 3.15;           // error
+    float normalizationFactor = 22.442e2; // float variable
+    char letter = 'a';   // character variable
+    char *name = "John"; // string variable
+
+
+    return 0;
+
+}
+
+
