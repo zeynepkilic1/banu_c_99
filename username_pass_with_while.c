bf0cc3bf410b69e8000eb4d4418f129a1909f255
zeynepkilic1 <119415722+zeynepkilic1@users.noreply.github.com>
2022-11-29 14:56:50 +0300

    Update username_pass_with_while.c

--- username_pass_with_while.c
+++ username_pass_with_while.c
@@ -1,1 +1,20 @@
-// cretae a username_pass_with_while. 
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+int main() {
+    int numbers[3] = {1, 2, 3, 4, 5};
+    int *ptr = numbers;
+    int i=0;
+
+    while (i<5)
+    {
+        printf("%d ", *(ptr + i));
+        i++;
+    }
+    
+    //for (int i = 0; i < 5; i++) {
+        //printf("%d ", *(ptr + i));
+   // }
+    return 0;
+}
