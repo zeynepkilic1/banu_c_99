2d771f0cf899a87315299998852a4834a9aac804
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-11-29 15:00:11 +0300

    Update username_pass_with_while.c
    The solution is shared.

--- username_pass_with_while.c
+++ username_pass_with_while.c
@@ -1,1 +1,33 @@
 // cretae a username_pass_with_while. 
+
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "ctype.h" // for isdigit()
+
+int main() {
+    char username[20];
+    char password[20];
+    char username1[20];
+    char password1[20];
+    int i = 0;
+    printf("Enter your username: ");
+    scanf("%s", username);
+    printf("Enter your password: ");
+    scanf("%s", password);
+    while (i < 3) {
+        printf("Enter your username: ");
+        scanf("%s", username1);
+        printf("Enter your password: ");
+        scanf("%s", password1);
+        if (strcmp(username, username1) == 0 && strcmp(password, password1) == 0) {
+            printf("You are logged in");
+            break;
+        } else {
+            printf("Wrong username or password");
+            i++;
+        }
+    }
+    return 0;
+}
