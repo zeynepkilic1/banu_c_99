ecb1bcc6610166a6217608787d778379bbd0554f
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-11-29 14:24:08 +0300

    Create 7.pointer_exercise.c

---
+++ 7.pointer_exercise.c
@@ -0,0 +1,14 @@
+
+//using pointer to change the value of a variable
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+int main() {
+    int a = 10;
+    int *p = &a;
+//   your code goes here
+    printf("%d", a);
+    return 0;
+}
