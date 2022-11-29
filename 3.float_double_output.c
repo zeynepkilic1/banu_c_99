bd673ef924517e126a74f8ac3e85b915d431d38b
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-11-29 09:42:16 +0300

    Create 3.float_double_output.c

---
+++ 3.float_double_output.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+//   float and double Output
+    float f = 1.0/3.0;
+    double d = 1.0/3.0;
+    printf("f = %f, d = %f  (float and double) \r \n", f, d);
+    printf("f = %e, d = %e  (float and double) \r \n", f, d);
+    printf("f = %g, d = %g  (float and double) \r \n", f, d);
+    printf("f = %a, d = %a  (float and double) \r \n", f, d);
+    printf("f = %A, d = %A  (float and double) \r \n", f, d);
+    printf("f = %E, d = %E  (float and double) \r \n", f, d);
+    printf("f = %G, d = %G  (float and double) \r \n", f, d);
+    printf("f = %F, d = %F  (float and double) \r \n", f, d);
+    printf("f = %lf, d = %lf  (float and double) \r \n", f, d);
+    printf("f = %le, d = %le  (float and double) \r \n", f, d);
+    printf("f = %lg, d = %lg  (float and double) \r \n", f, d);
+    printf("f = %la, d = %la  (float and double) \r \n", f, d);
+    printf("f = %lA, d = %lA  (float and double) \r \n", f, d);
+    printf("f = %lE, d = %lE  (float and double) \r \n", f, d);
+    printf("f = %lG, d = %lG  (float and double) \r \n", f, d);
+    printf("f = %lF, d = %lF  (float and double) \r \n", f, d);
+    printf("f = %Lf, d = %Lf  (float and double) \r \n", f, d);
+    printf("f = %Le, d = %Le  (float and double) \r \n", f, d);
+    
+    return 0;
+}
+
