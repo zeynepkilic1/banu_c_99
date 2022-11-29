e408187379bbb6885f2b8003161b290d41d8fa72
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-11-29 14:38:27 +0300

    Update 8.pointer_more.c

--- 8.pointer_more.c
+++ 8.pointer_more.c
@@ -3,13 +3,14 @@
 #include "string.h"
 
 int main() {
-    int a = 10;
-    int *p = a;
-    *p = 20;
-    printf("%d", a);
-    printf("%d", *p);
-    printf("%d", &a);
-    printf("%d", p);
+    int age= 21;
+    int *ptr = age;
+    
+    *ptr = 20;
+    printf("%d", age);
+    printf("%d", *ptr);
+    printf("%d", &age);
+    printf("%d", ptr);
     return 0;
 
 }
