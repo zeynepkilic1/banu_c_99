576393007413f84eeb83554bd6aa3b00b2bbafe9
zeynepkilic1 <119415722+zeynepkilic1@users.noreply.github.com>
2022-12-03 18:49:37 +0300

    Add files via upload

---
+++ userName_password.c
@@ -0,0 +1,45 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "ctype.h" 
+
+struct login
+{
+    char username[20];
+    char password[20];
+
+};
+
+
+void login(struct login log) {
+    char username1[20];
+    char password1[20];
+
+    printf("enter your username: ");
+    scanf("%s",username1);
+
+    printf("enter your password: ");
+    scanf("%s",password1);
+
+    if ((strcmp(log.username,username1)==0) && (strcmp(log.password,password1)==0))
+    {
+        printf("you are logged in");
+    }
+    else
+    {
+        printf("try again");
+    }
+}
+
+int main() {
+    
+    struct login log;
+    printf("Enter your username: ");
+    scanf("%s", log.username);
+    printf("Enter your password: ");
+    scanf("%s", log.password);
+    login(log);
+    return 0;
+}
+
+
